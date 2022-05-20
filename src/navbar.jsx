7edef574398443f6b04d95d39efb0cb9b515b1e1
VishalPawar3696 <vishalpawar3696@gmail.com>
2022-05-20 18:38:11 +0530

    first commit

---
+++ src/navbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function navbar() {
+  return (
+  
+
+  
+    
+     <navbar id="navbar">
+      <h2>LOGOBAKERY</h2>
+      <div class="link">
+        <p>services</p>
+        <p>Projects</p>
+        <p>About</p>
+      </div>
+      <button id="btn">Contact</button>
+  
+    </navbar>
+    
+
+  );
+}
+
+export default navbar;
